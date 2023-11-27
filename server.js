8edb7cff7f8f72dd7c66bd0854d2ce2769b3023e
makspryme <makspryme17@gmail.com>
2023-11-27 20:30:57 +0200

    done

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import app from './app.js';
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+const DB_HOST =
+  'mongodb+srv://makspryme17:7luvJSloIjxm8cYO@cluster0.rla1ny9.mongodb.net/NodeJS?retryWrites=true&w=majority';
 
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
+    process.exit(1);
+  });
