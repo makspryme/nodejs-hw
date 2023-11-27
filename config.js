f89ceefcce31f977376066f8c6c8cb7029f45ddd
makspryme <makspryme17@gmail.com>
2023-11-27 20:41:51 +0200

    add config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const DB_HOST =
+  'mongodb+srv://makspryme17:7luvJSloIjxm8cYO@cluster0.rla1ny9.mongodb.net/NodeJS?retryWrites=true&w=majority';
+
+export default DB_HOST;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import app from './app.js';
 import mongoose from 'mongoose';
-
-const DB_HOST =
-  'mongodb+srv://makspryme17:7luvJSloIjxm8cYO@cluster0.rla1ny9.mongodb.net/NodeJS?retryWrites=true&w=majority';
+import DB_HOST from './config.js';
 
 mongoose
   .connect(DB_HOST)
